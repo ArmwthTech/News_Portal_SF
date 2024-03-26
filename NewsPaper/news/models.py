3a88a9ccafd82f9ed83e419ee1cb7b8681c96206
ArmWthTech <98inarik@gmail.com>
2024-03-26 23:11:01 +0500

    new_Models
    add new models for my postnews

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+
+    def update_rating(self):
+        articles_rating = sum(post.rating for post in Post.objects.filter(author=self))
+        article_comments_rating = sum(comment.rating for post in Post.objects.filter(author=self, type='article').prefetch_related('comment_set').all() for comment in Comment.objects.filter(post=post))
+        all_comments_rating = sum(comment.rating for comment in Comment.objects.filter(user=self))
+        self.rating = articles_rating * 3 + article_comments_rating + all_comments_rating
+        self.save()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    TYPE_CHOICES = (
+        ('article', 'Article'),
+        ('news', 'News'),
+    )
+    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    categories = models.ManyToManyField(Category, through='PostCategory')
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    rating = models.IntegerField(default=0)
+
+    def preview(self):
+        return self.content[:124] + '...' if len(self.content) > 124 else self.content
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
