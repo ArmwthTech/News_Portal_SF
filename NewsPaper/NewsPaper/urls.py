29d3dd4c902aaf212bebbbabc8c79e2b4a451d5d
ArmWthTech <98inarik@gmail.com>
2024-04-02 21:10:10 +0500

    new

--- NewsPaper/NewsPaper/urls.py
+++ NewsPaper/NewsPaper/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('news/', views.news_list, name='news_list'),
+    path('news/<int:pk>/', views.news_detail, name='news_detail'),
 ]
--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+def news_list(request):
+    news = Post.objects.all()
+    return render(request, 'news/news_list.html', {'posts': news})
+
+def news_detail(request, id):
+    news_item = get_object_or_404(Post, pk=pk)
+    return render(request, 'news/news_detail.html', {'post': news_item})
